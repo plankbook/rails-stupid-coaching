b3f62923b647bb6fbf8655e8da6887bb132579fc
Emre Ozen <fossoyeuse@proton.me>
2023-04-04 20:41:06 -0400

    donezo

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,7 +3,14 @@ class QuestionsController < ApplicationController
   end
 
   def answer
-    @response = params[:question]
+    @input = params[:question]
     # raise
+    if @input == 'I am going to work'
+      then @response = 'Great'
+    elsif @input.end_with?('?')
+      then @response = 'Silly question, get dressed and go to work!'
+    else
+      @response = 'I don\'t care, get dressed and go to work!'
+    end
   end
 end
